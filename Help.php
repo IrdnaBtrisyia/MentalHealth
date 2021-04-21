8a6e60439d670ec4634a40d3670659a969ce4dad
Irdina Batrisyia <80381951+IrdnaBtrisyia@users.noreply.github.com>
2021-04-21 22:48:56 +0800

    Add files via upload

---
+++ Help.php
@@ -0,0 +1,56 @@
+<HTML>
+<head>
+<style>
+
+body {
+	margin: 0;
+	padding: 50px;
+	text-align: center;
+	background: url(https://media.bizj.us/view/img/11636312/gettyimages-1168894271*1200xx4861-2748-0-1054.jpg) ;
+	background-size: cover;
+	font-family:  Arial, Helvetica, sans-serif;
+	color: white;
+	}
+	
+a{
+	border-color: transparent;
+	color: #fff;
+	font-size: 20px;
+	font-weight: bold;
+	letter-spacing: 1.5px;
+	height: 50px;
+	margin-top: 20px;
+	}
+	
+	
+</style>
+</head>
+<body>
+
+<?php
+
+	$input = $_POST ['name'];
+	
+?>
+
+<p style="text-align: center;"><img src="https://cdn.jotfor.ms/img/check-icon.png" alt="" width="128" height="128" /></p>
+<div style="text-align: center;">
+<h1 style="text-align: center;">Thank You!</h1>
+<p>
+
+<?php
+Print "$input";
+?>
+
+</p>
+<p style="text-align: center;">Your message has been sent. We will be with you in a sec. </p>
+</div>
+
+<div>
+	<a href="https://irdnabtrisyia.github.io/OpenSourceProject/index">Home</a></li>
+</div>
+
+
+
+</body>
+</HTML>
